5d9d9cfe1af6c7687b48f2686584a593f122ac76
Artem Dudarev <dudarev@gmail.com>
2018-02-09 23:00:38 +0200

    Added STATIC_ROOT

--- policyinsights/settings/heroku.py
+++ policyinsights/settings/heroku.py
@@ -11,3 +11,8 @@ DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 # https://warehouse.python.org/project/whitenoise/
 MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware', ] + MIDDLEWARE
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
