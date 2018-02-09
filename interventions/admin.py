5e40af896b0564fe67726b56b3175b8fd03ea6ee
Artem Dudarev <dudarev@gmail.com>
2018-02-09 23:18:56 +0200

    Show location column for interventions in admin

--- interventions/admin.py
+++ interventions/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from interventions.models import Intervention, Location
 
 
-admin.site.register(Intervention)
+class InterventionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location',)
+
+admin.site.register(Intervention, InterventionAdmin)
 admin.site.register(Location)
