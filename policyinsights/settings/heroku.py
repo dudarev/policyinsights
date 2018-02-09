015995d6851abe98c06f62713e1439b1b3f32340
Artem Dudarev <dudarev@gmail.com>
2018-02-09 22:50:07 +0200

    Whitenoise to serve static files

--- policyinsights/settings/heroku.py
+++ policyinsights/settings/heroku.py
@@ -1,3 +1,4 @@
+# https://devcenter.heroku.com/articles/django-app-configuration
 import dj_database_url
 
 from .base import *
@@ -5,3 +6,8 @@ from .base import *
 
 # Change 'default' database configuration with $DATABASE_URL.
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+MIDDLEWARE_CLASSES = ('whitenoise.middleware.WhiteNoiseMiddleware', ) + MIDDLEWARE_CLASSES
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
