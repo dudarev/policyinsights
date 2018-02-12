d2198a1c2644f5488c671e35cd490c83cbed42c3
Artem Dudarev <dudarev@gmail.com>
2018-02-12 12:19:23 +0200

    Introduced InterventionCategory

--- interventions/admin.py
+++ interventions/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from interventions.models import Intervention, Location
+from interventions.models import InterventionCategory, Intervention, Location
+
+
+class InterventionCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location',)
 
 
 class InterventionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'location',)
+    list_display = ('name', 'category',)
+
 
 admin.site.register(Intervention, InterventionAdmin)
+admin.site.register(InterventionCategory, InterventionCategoryAdmin)
 admin.site.register(Location)
--- interventions/models.py
+++ interventions/models.py
@@ -8,8 +8,19 @@ class Location(models.Model):
         return self.name
 
 
-class Intervention(models.Model):
+class InterventionCategory(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name_plural = "Intervention categories"
+
+    def __str__(self):
+        return '{}, {}'.format(self.name, self.location.name)
+
+
+class Intervention(models.Model):
+    category = models.ForeignKey(InterventionCategory, on_delete=models.CASCADE)
     name = models.CharField(max_length=300)
 
     def __str__(self):
