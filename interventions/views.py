86149dd913dfdc929f6cedfab4b00a4016122d18
Artem Dudarev <dudarev@gmail.com>
2018-02-12 14:22:02 +0200

    User may search for location

--- interventions/views.py
+++ interventions/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Location
+
+
+class LocationsSearchView(ListView):
+
+    model = Location
+
+    def get_queryset(self):
+        queryset = Location.objects.all()
+        if self.request.GET.get('q'):
+            queryset = queryset.filter(name__contains=self.request.GET.get('q'))
+        return queryset
+
+
+class LocationDetail(DetailView):
+
+    model = Location
--- policyinsights/urls.py
+++ policyinsights/urls.py
@@ -19,10 +19,13 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.views.generic import TemplateView
+from interventions import views as intervention_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('locations/search', intervention_views.LocationsSearchView.as_view(), name='location-search'),
+    path('locations/<int:pk>', intervention_views.LocationDetail.as_view(), name='location-detail'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url('^$', TemplateView.as_view(template_name='index.html'), name='home'),
