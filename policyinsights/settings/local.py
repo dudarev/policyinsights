61d5a4db65b1a5d2341250053fad2a6dcf51c44d
Artem Dudarev <dudarev@gmail.com>
2018-02-26 18:44:46 +0200

    Use PostgreSQL for local development

--- policyinsights/settings/local.py
+++ policyinsights/settings/local.py
@@ -4,3 +4,11 @@ from .base import *
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 SITE_ID = 1  # localhost:8000
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'policyinsights',
+        'USER': 'policyinsights',
+    }
+}
