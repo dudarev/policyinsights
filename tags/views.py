6bd02e3c7b0100cfff043e3e6a9d018e15c9f0e9
Artem Dudarev <dudarev@gmail.com>
2018-02-27 23:02:46 +0200

    If tag does not exist, redirect to page to create it

--- tags/views.py
+++ tags/views.py
@@ -1,12 +1,13 @@
+from django.shortcuts import redirect
 from django.views.generic import DetailView, UpdateView, CreateView, ListView
 from django.urls import reverse
+from django.http import Http404
 
 from .models import Tag
 from .forms import TagForm
 
 
 class TagCreateUpdateMixin():
-
     model = Tag
     form_class = TagForm
 
@@ -15,27 +16,21 @@ class TagCreateUpdateMixin():
 
 
 class TagCreate(TagCreateUpdateMixin, CreateView):
-
-    pass
+    def get_initial(self):
+        return {'slug': self.request.GET.get('slug', '')}
 
 
 class TagDetail(DetailView):
-
     model = Tag
 
-    def get_context_data(self, **kwargs):
-        context = super(TagDetail, self).get_context_data()
-        # TODO: get programs for tag or do not implement?
-        # programs = self.object.program_set.all()
-        # context.update({'programs': programs})
-        return context
+    def get(self, request, *args, **kwargs):
+        try:
+            self.object = self.get_object()
+        except Http404:
+            return redirect("{}?slug={}".format(reverse('tag-create'), self.kwargs.get('slug', '')))
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
 
 
 class TagUpdate(TagCreateUpdateMixin, UpdateView):
-
     pass
-
-
-class TagList(ListView):
-
-    model = Tag
