e6422bba933e4f162a10f1ca2b2e5bfcad305edd
Artem Dudarev <dudarev@gmail.com>
2018-03-20 07:00:40 +0200

    For testing geventpool locally

--- policyinsights/settings/local.py
+++ policyinsights/settings/local.py
@@ -13,3 +13,26 @@ DATABASES = {
         'PASSWORD': 'admin',
     }
 }
+
+
+""" for testing django_db_geventpool
+# https://github.com/jneight/django-db-geventpool
+# for django 1.6 and newer version, CONN_MAX_AGE must be set to 0, or connections will never go back to the pool
+DATABASES['default'].update({
+    'ENGINE': 'django_db_geventpool.backends.postgresql_psycopg2',
+    'ATOMIC_REQUESTS': False,
+    'CONN_MAX_AGE': 0,
+    'OPTIONS': {
+        'MAX_CONNS': 4,  # 3 workers, 4 connections each, leaving 8 connections out of 20 for one-off tasks
+    }})
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware', ] + MIDDLEWARE
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "policyinsights/static/"),
+]
+"""
