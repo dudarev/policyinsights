ab2c74dde476b2b655efa1f2546f6c75b53944b7
Artem Dudarev <dudarev@gmail.com>
2018-03-20 06:59:34 +0200

    Note about geventpool

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,4 @@
+# based on https://medium.com/@bfirsh/squeezing-every-drop-of-performance-out-of-a-django-app-on-heroku-4b5b1e5a3d44
 # https://github.com/jneight/django-db-geventpool
 from psycogreen.gevent import patch_psycopg     # use this if you use gevent workers
 
