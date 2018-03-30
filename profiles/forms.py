d51e6abd7c099f8c6432f86ea11917752d6dfdfd
Artem Dudarev <dudarev@gmail.com>
2018-03-30 12:26:11 +0300

    Mention that username is a public field during registration.

--- profiles/forms.py
+++ profiles/forms.py
@@ -8,6 +8,12 @@ class CustomRegistrationForm(RegistrationForm):
     required_css_class = 'required'
     zip_code = forms.CharField(min_length=5, max_length=5, required=True, label=_("ZIP code"))
 
+    class Meta(RegistrationForm.Meta):
+        help_texts = {
+            'username': 'Required. 150 characters or fewer.' + \
+                        'This is a public field. Avoid to use your email address if you do not want it to be shown.',
+        }
+
     def save(self, commit=True):
         user = super().save(commit=commit)
         user.profile.zip_code = self.cleaned_data['zip_code']
