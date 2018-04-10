b243ad6b97477d68fb6a4a2b1fb5b5d9fb0fdfa8
Artem Dudarev <dudarev@gmail.com>
2018-04-10 09:00:31 +0300

    Make sure profile view works for non-authenticated user

--- profiles/views.py
+++ profiles/views.py
@@ -9,10 +9,13 @@ class ProfileDetail(DetailView):
     model = Profile
 
     def get_object(self, queryset=None):
-        return Profile.objects.get_or_create(user=self.request.user)
+        if self.request.user.is_authenticated:
+            return Profile.objects.get_or_create(user=self.request.user)
+        return None
 
     def get_context_data(self, **kwargs):
-        context = super(ProfileDetail, self).get_context_data()
-        user_programs = UserProgram.objects.filter(user=self.request.user).all()
-        context.update({'user_programs': user_programs})
+        context = super(ProfileDetail, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            user_programs = UserProgram.objects.filter(user=self.request.user).all()
+            context.update({'user_programs': user_programs})
         return context
